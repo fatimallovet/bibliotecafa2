362431ece4d39c7fc6f336b70e3c5a3b03561727
fatimallovet <fatimallovet@gmail.com>
2025-10-28 21:15:42 -0600

    actualiz gid sheets

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR_lN4MQGP2PigjKJFOV8ZK92MvfpQWj8aH7qqntBJHOKv6XsvLAxriHmjU3WcD7kafNvNbj3pTFqND/pub?gid=<GID_ESP>&single=true&output=csv";
+const sheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR_lN4MQGP2PigjKJFOV8ZK92MvfpQWj8aH7qqntBJHOKv6XsvLAxriHmjU3WcD7kafNvNbj3pTFqND/pub?gid=1357667123&single=true&output=csv";
 
 function showError(msg){
   console.error(msg);
